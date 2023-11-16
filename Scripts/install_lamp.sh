38dff992c429beb2e11dc49ac44f750fd84988d6
samuel <samuel@gmail.com>
2023-11-16 09:31:58 +0000

    "LAMP"

--- Scripts/install_lamp.sh
+++ Scripts/install_lamp.sh
@@ -15,13 +15,48 @@ dnf update
 
 dnf install httpd -y
 
+# Iniciar el servidor httpd:
+
+systemctl start httpd
+
+# Arrancar automaticamente:
+
+systemctl enable httpd
+
 # instalar sgbd mysql
 
 dnf install -y mariadb105
 
 #Instalacion del php
 
-dnf install php-mbstring php-zip php-json php-gd php-fpm php-xml -y
+dnf install php-mbstring php-zip php-mysqli php-json php-gd php-fpm php-xml -y
+
+# Antes de nada pasamos con la eliminación de descargas previas del phpmyadmin:
+#------------------------------------------------------------------------------
+rm -rf /tmp/phpMyAdmin-5.2.1-all-languages.zip
+
+rm -rf /var/www/html/phpmyadmin
+#------------------------------------------------------------------------------
+
+# Obtener codigo fuente en la carpeta temporal:
+
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip -P /tmp
+
+# Instalo el unzip para descomprimir:
+
+dnf install unzip -y
+
+#descompresion en la carpeta /tmp
+
+unzip -u /tmp/phpMyAdmin-5.2.1-all-languages.zip -d /var/www/html
+
+#persnalizar el nombre
+
+mv /var/www/html/phpMyAdmin-5.2.1-all-languages /var/www/html/phpmyadmin
+
+#cambiar propietario y grupo:
+
+chown -R apache:apache /var/www/html
 
 #Copiar el archivo de conf de apache
 
